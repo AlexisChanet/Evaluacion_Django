b9498f7779b16ec7898f6fbd164371442ce6c9da
AlexisChanet <alexis.castillo20@inacapmail.cl>
2026-09-24 19:15:46 -0300

    Cambios finales, navbar, imagen de motocicleta generica

--- motos/admin.py
+++ motos/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Motocicleta
 
-# Register your models here.
+@admin.register(Motocicleta)
+class MotocicletaAdmin(admin.ModelAdmin):
+    # Columnas que se verán en el panel de administrador
+    list_display = ('marca', 'modelo', 'anio', 'precio', 'stock')
+    # Permite buscar motos por marca o modelo
+    search_fields = ('marca', 'modelo')
+    # Permite filtrar en el menú lateral por marca o año
+    list_filter = ('marca', 'anio')
